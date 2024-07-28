661d7e501c59285621cc5ba920ed91544f0ad914
chand <chand@LAPTOP-DNU0L4GM>
2024-07-28 11:42:52 +0530

    change the code by dev

--- GITProj01_Basic/src/com/cs/Hello_team.java
+++ GITProj01_Basic/src/com/cs/Hello_team.java
@@ -4,6 +4,10 @@ public class Hello_team {
 
 	public static void main(String[] args) {
 		System.out.println("code ni chedagottoddura dev");
+		for(int i:new int[]{1,2,3,4,5})
+		{
+			System.out.print(i+" ");
+		}
 	}
 
 }
