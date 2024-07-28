7dd1aa5969d2d4bc811e556c395a2ddb7cf52732
chand <chand@LAPTOP-DNU0L4GM>
2024-07-28 11:31:15 +0530

    dev added the code to project

---
+++ GITProj01_Basic/src/com/cs/Hello_team.java
@@ -0,0 +1,9 @@
+package com.cs;
+
+public class Hello_team {
+
+	public static void main(String[] args) {
+		System.out.println("hola! genki desta");
+	}
+
+}
--- GITProj01_Basic/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module GITProj01_Basic {
-}
